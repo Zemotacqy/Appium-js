38f6999a8ad091e40fffb2d73cadb3b150a65cff
Manish Ranjan <mranjan1398@gmail.com>
2020-03-07 19:15:20 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,8 @@ echo "Welcome to Appium-js"
 echo "Hllo world"
 echo "Building APk"
 op="This is a output"
-
+echo $ANDROID_HOME
+echo $ANDROID_SDK_ROOT
 
 echo "${INPUT_BS_USERNAME}"
 echo "${INPUT_BS_ACCESSKEY}"
